d00cdc16d218c2518cc417321f153bd954f287ce
Maron-X <120732521+Maron-X@users.noreply.github.com>
2024-04-25 18:16:43 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  async asyncData({ app, store }) {},
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ store/index.js
@@ -0,0 +1,9 @@
+export const state = () => ({})
+
+export const getters = {}
+
+export const actions = {
+  getAllDataFromApi({ commit }, payload) {},
+}
+
+export const mutations = {}
